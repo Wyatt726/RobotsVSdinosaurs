aee3f9f6d79788ba329aa7fd25c7c75b3c0140f5
wyatt Orman <wyattorman@wyatts-Air.lan>
2021-08-11 17:21:15 -0400

    saving changes ot fleet anf herd

--- fleet.py
+++ fleet.py
@@ -1,6 +1,6 @@
 class Fleet:
     def __init__(self):
-        new_weapon = Weapon("Sword", 25)
+        new_weapon = weapon("Sword", 25)
         my_robot = Robot("Wall-E", 100, new_weapon)
         my_robot1 = Robot("T-800", 100, new_weapon)
         my_robot2 = Robot("Bob", 100, new_weapon)
--- herd.py
+++ herd.py
@@ -1,6 +1,6 @@
 class Herd:
     def __init__(self):
-        new_weapon = Weapon("laser", 50)
+        new_weapon = weapon("laser", 50)
         my_dino = dino("Tiny-Diny", 100, new_weapon)
         my_dino1 = dino("Yellow-Diny", 100, new_weapon)
         my_dino2 = dino("Orange-Diny", 100, new_weapon)
