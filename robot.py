2b3fe29979502185c663d07a10687e181bbce3c4
wyatt Orman <wyattorman@wyatts-Air.lan>
2021-08-11 14:53:56 -0400

    class update and change added in wpn class

---
+++ robot.py
@@ -0,0 +1,38 @@
+class robot:
+    def __init__(self):
+        self.robot_name = ''
+        self.robot_health = '' #100pts or full?
+        self.robot_weapon = '' #robo_blaster
+      
+    
+    def robot_details(self, robot_name, robot_health, robot_weapon):
+        self.robot_name = 'robo_robot'
+        self.robot_health = 'Full'
+        self.robot_weapon = 'robo_blaster'
+
+class robo_blaster:
+    def __init__(self):
+        self.wpn_type = ''
+        self.round_count = ''
+        self.hit_points = ''
+
+
+class dino:
+        def __init__ (self):
+            self.dino_name = ''
+            self.dino_health = ''
+            self.dino_weapon = ''
+         
+        
+
+        def dino_details(self, dino_name, dino_health, dino_weapon):
+            self.dino_name = dino_man
+            self.dino_health = full
+            self.dino_weapon = flinstone_cannon
+        
+class flinstone_cannon:
+     def __inint__ (self):
+            self.wpn_type = 'cannon_pistol'
+            self.round_count = ''
+            self.hit_points = ''
+
